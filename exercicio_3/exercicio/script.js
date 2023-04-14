1c468c52419ea1b265ded8e2b96e6baae985eb1e
RicardoGuia <51034291+Ricardo-Guia@users.noreply.github.com>
2023-04-14 17:31:37 +0100

    complete exercice 3

--- exercicio_3/exercicio/script.js
+++ exercicio_3/exercicio/script.js
@@ -37,5 +37,21 @@ function requestFlickrApi() {
 }
 
 function requestMeteoApi() {
+    const cityValue = $('#inputMeteoID').val();
 
+    $.ajax({
+        url: 'https://api.openweathermap.org/data/2.5/weather',
+        data: {
+            q: cityValue,
+            appid: 'a9e49b305a5d14cc0790dbf507251ca1',
+            units: 'metric',
+            lang: 'PT'
+        },
+        method: 'GET',
+        success: function (response) {
+            console.log(response);
+            const temperature = response.main.temp;
+            $('#meteoContainer').html('A temperatura em ' + response.name + ' é de: ' + temperature);
+        }
+    });
 }
