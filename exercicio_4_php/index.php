11f5cf597620e54318d0b6ac6d926b50c0d3ddb4
rafacabral27 <126591479+rafacabral27@users.noreply.github.com>
2023-05-30 17:46:58 +0100

    Create index.php

---
+++ exercicio_4_php/index.php
@@ -0,0 +1,13 @@
+<?php
+
+echo 'ola mundo no dia' . date("m.d.y");
+
+?>  
+
+<br>
+
+<form style="margin-top: 200px" method="GET">
+    Escreva um número:
+    <input type="number" name="numeroUser" >
+    <button type="submit">
+</form>
